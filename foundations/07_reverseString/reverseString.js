2b3f3ec58028dee55fb09ce05b6ff986bcb618fc
arretez95 <73412535+arretez95@users.noreply.github.com>
2026-07-14 21:41:56 -0700

    Added logic to split, reverse, then join string

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let str = string.split('')
+    str.reverse()
+    let newStr = str.join('')
+    return newStr
 };
 
+
+console.log(reverseString("hello"))
 // Do not edit below this line
 module.exports = reverseString;
