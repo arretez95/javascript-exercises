cf3df16305edcc8e4442aca6faa4ef86a5493b6d
arretez95 <73412535+arretez95@users.noreply.github.com>
2026-07-14 23:06:27 -0700

    Get index of unwanted array and delete

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, el) {
+    let index = arr.indexOf(el)
+    arr.splice(index, 1)
+    return arr
 };
+
+console.log(removeFromArray([1,2,3,4], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
