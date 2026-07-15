e75d6771e8f62e1a4d58a51a24375ed48aba5b72
arretez95 <73412535+arretez95@users.noreply.github.com>
2026-07-14 21:29:33 -0700

    Removed debugger & skips from test

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,12 +1,10 @@
 const repeatString = function(string, num) {
-    let repeat;
+    let repeat = "";
     for ( let i = 0; i < num; i++) {
         repeat += string;
     }
     return repeat
 };
-
-console.log(repeatString("hey",3))
 
 // Do not edit below this line
 module.exports = repeatString;
