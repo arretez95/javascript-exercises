e29643a88a5bef13a789d83fa56098eb0cfdc64c
arretez95 <73412535+arretez95@users.noreply.github.com>
2026-07-14 23:37:28 -0700

    Changed for argument to take from el array

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,13 +1,22 @@
-const removeFromArray = function(arr, ...el) {
-    let newArr = []
+/* const removeFromArray = function(arr, ...el) {
     for (const arg of el) {
-        let index = arr.indexOf(el)
-        newArr = arr.splice(index, 1)
+        let index = arr.indexOf(el) - 1
+        arr.splice(index, 1)
+        index = ""
     }
-    return newArr    
-};
+    return arr
+}; */
+const removeFromArray = function(arr, ...el) {
+    let newArr = [];
+    arr.forEach( item => {
+        if (!el.includes(item)) {
+            newArr.push(item)
+        }
+    })
+    return newArr
+}
 
-console.log(removeFromArray([1,2,3,4], 3));
+console.log(removeFromArray([1,2,3,4], 3, 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
