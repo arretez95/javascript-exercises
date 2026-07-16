c272ced03e3176c4402a8284e7c3ed9fc66354c0
arretez95 <73412535+arretez95@users.noreply.github.com>
2026-07-15 21:02:23 -0700

    Rounded numbers now return as strings

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,25 +1,25 @@
 const convertToCelsius = function(temp) {
   // celsius = (fahrenheit - 32) * (5/9)
-  let fToC = (temp - 32) * (5/9)
+  let fToC = (temp - 32) * (5/9);
    if (fToC - Math.floor(fToC) != 0) {
-    return fToC.toFixed(1)
+    return Number(fToC.toFixed(1));
   } else {
-    return fToC
+    return fToC;
   }
 };
 
 const convertToFahrenheit = function(temp) {
   // fahrenheit = celsius * 9/5 + 32
-  let cToF = (temp * 9/5  + 32) 
+  let cToF = temp * 9/5  + 32;
   if (cToF - Math.floor(cToF) != 0) {
-    return cToF.toFixed(1)
+    return Number(cToF.toFixed(1));
   } else {
-    return cToF
+    return cToF;
   }
 };
 
-console.log( convertToCelsius(100) )
-console.log( convertToFahrenheit(0))
+console.log( convertToCelsius(100) );
+console.log( convertToFahrenheit(0));
 
 // Do not edit below this line
 module.exports = {
