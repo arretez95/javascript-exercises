b96394d3da9dfd12a930b83fd84a8d5f78a27978
arretez95 <73412535+arretez95@users.noreply.github.com>
2026-07-15 19:28:46 -0700

    Added formulas for converting between C and F

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  // celsius = (fahrenheit - 32) * (5/9)
+  return ((temp - 32) * (5/9)).toFixed(1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  // fahrenheit = celsius * 9/5 + 32
+  return (temp * 9/5  + 32).toFixed(1)
 };
+
+console.log( convertToCelsius(32))
+console.log( convertToFahrenheit(0))
 
 // Do not edit below this line
 module.exports = {
