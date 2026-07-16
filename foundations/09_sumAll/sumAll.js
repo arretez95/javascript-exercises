900e8c192b42706f2ecd65c31b0738efed8b5fa4
arretez95 <73412535+arretez95@users.noreply.github.com>
2026-07-15 18:53:22 -0700

    Add error checks for non integer and nonnumber values

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,7 @@
 const sumAll = function(x, y) {
     if ( !x || !y || x < 0 || y < 0 ) return "ERROR"
+    if (typeof x != "number" || typeof y != "number") return "ERROR"
+    if ( x - Math.floor(x) == 0) return "ERROR"
     
     let a = Math.min(x, y)
     let b = Math.max(x, y)
