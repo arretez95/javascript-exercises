3ef764e1f8d04e160a5481f8e7d97713b1dabc89
arretez95 <73412535+arretez95@users.noreply.github.com>
2026-07-15 18:48:18 -0700

    Fixed interval to make y inclusive

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -5,10 +5,11 @@ const sumAll = function(x, y) {
     let b = Math.max(x, y)
     let arr = [];
 
-    arr = Array.from( {length: b - a},
+    arr = Array.from( {length: b - a + 1},
                 (v, i) => a + i,
     )
-    return arr
+    
+    return arr.reduce( (v, i) => v + i, 0)
 };
 
 console.log( sumAll(1, 4))
