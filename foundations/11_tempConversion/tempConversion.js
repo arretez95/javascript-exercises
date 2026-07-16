06285035fab6139b64c85d57315b81038227b3be
arretez95 <73412535+arretez95@users.noreply.github.com>
2026-07-15 19:34:02 -0700

    If not integer, round to 1 decimal place

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,14 +1,24 @@
 const convertToCelsius = function(temp) {
   // celsius = (fahrenheit - 32) * (5/9)
-  return ((temp - 32) * (5/9)).toFixed(1)
+  let fToC = (temp - 32) * (5/9)
+   if (fToC - Math.floor(fToC) != 0) {
+    return fToC.toFixed(1)
+  } else {
+    return fToC
+  }
 };
 
 const convertToFahrenheit = function(temp) {
   // fahrenheit = celsius * 9/5 + 32
-  return (temp * 9/5  + 32).toFixed(1)
+  let cToF = (temp * 9/5  + 32) 
+  if (cToF - Math.floor(cToF) != 0) {
+    return cToF.toFixed(1)
+  } else {
+    return cToF
+  }
 };
 
-console.log( convertToCelsius(32))
+console.log( convertToCelsius(100) )
 console.log( convertToFahrenheit(0))
 
 // Do not edit below this line
