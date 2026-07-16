5a382cdd0f6e1ae2e485b44f1b7e7443e601fdaf
arretez95 <73412535+arretez95@users.noreply.github.com>
2026-07-15 19:09:22 -0700

    Years divided by 4 are leap years

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    return (year % 4) === 0 ? true : false
 };
+
+console.log( leapYears(2000) )
+console.log( leapYears(1985) )
 
 // Do not edit below this line
 module.exports = leapYears;
