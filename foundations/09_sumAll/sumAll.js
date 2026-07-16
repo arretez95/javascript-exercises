065a45a34d1fa7640aad2c153e14e9b3da398e25
arretez95 <73412535+arretez95@users.noreply.github.com>
2026-07-15 18:46:38 -0700

    Fixed bug wherelength would calculate in negatives

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,7 +1,17 @@
 const sumAll = function(x, y) {
-    if ( !x || !y ) return "ERROR"
-    if ( x < 0 || y < 0 ) return "ERROR"
+    if ( !x || !y || x < 0 || y < 0 ) return "ERROR"
+    
+    let a = Math.min(x, y)
+    let b = Math.max(x, y)
+    let arr = [];
+
+    arr = Array.from( {length: b - a},
+                (v, i) => a + i,
+    )
+    return arr
 };
+
+console.log( sumAll(1, 4))
 
 // Do not edit below this line
 module.exports = sumAll;
